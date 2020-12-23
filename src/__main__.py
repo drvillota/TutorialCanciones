0c10de341d31a020c21516544a301ef1f129860e
avargas20 <a.vargas20@uniandes.edu.co>
2020-12-23 15:04:38 -0500

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,3 @@
-from src.logica.Coleccion import Coleccion
-from src.modelo.album import Album, Medio
-from src.modelo.cancion import Cancion
-from src.modelo.interprete import Interprete
 from src.modelo.declarative_base import session, Base, engine
 
 if __name__ == '__main__':
